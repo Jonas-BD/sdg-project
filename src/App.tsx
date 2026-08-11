20a0fce7dd2758c70d5a8dc66f717c0d476d8e63
Jonas <joda12@elev.techcollege.dk>
2026-08-11 10:31:32 +0200

    Added components

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Header } from './Components/Header/Header'
+import { Button } from './Components/Button/Button'
+import { Footer } from './Components/Footer/Footer'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Button />
+    <Footer />
+    </>
+  )
+}
+
+export default App
